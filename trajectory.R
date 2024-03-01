7d2848359145b7216693c3a5d3e4db589e85fb3c
m7668417 <m.m.voets@utwente.nl>
2024-03-01 11:28:43 +0100

    switch DS/US branches

--- trajectory.R
+++ trajectory.R
@@ -2,9 +2,15 @@
 library(simmer);
 library(simmer.plot);
 library(dplyr)
+library(DiagrammeRsvg)
+library(rsvg)
+
+t_img <- 0
+t_biopsy <- 0
+t_fn <- 0
 
 
-out_trj <- trajectory() %>% 
+out.trj <- trajectory() %>% 
   set_attribute(
     keys   = c("time_of_death"), 
     values = function() fn_time_to_events(
@@ -13,15 +19,13 @@ out_trj <- trajectory() %>%
     )
   )
 
-fup_trj <- trajectory() %>% 
+fup.trj <- trajectory() %>% 
   set_attribute(keys = "start_time", values = function() now(.env = sim)) %>% 
   renege_in(t = function() now(.env = sim) + t_to_death_oc, out = out_trj) %>% 
-  #set_attribute("state", "Disease free") %>%
+  #set_attribute(key="state", "DF") %>%
+  set_attribute(key="state_DF", values=1) %>%
   
-  #TO DO: insert counter years of surveillance
-  set_attribute(key="surv.year", value=0) %>%
-  
-  # Time to first imaging event
+   # Time to first imaging event
   #timeout(task = t_first_fup) %>% #distribution to follow-up event
   
   # Follow-up imaging event
@@ -32,30 +36,95 @@ fup_trj <- trajectory() %>%
     mod = '+'
   ) %>%
   #timeout(task = t_fup) %>%
-  release(resource = "Imaging")
+  release(resource = "Imaging") %>%
+  
+  #TO DO: insert counter years of surveillance
+  set_attribute(key="surv.year", value=0) %>%
 
   #first branch, based on what the outcome is of the imaging event
-  branch(option = function() fn_img_event(get_attribute(sim, "mod")), continue = c(T,T,T,F,T), 
+  branch(option = function() fn_img_event(get_attribute(sim, "mod")), continue = c(T,T,T,T), #,F,T), 
        #Event 1: True Negative
        trajectory() %>%
          set_attribute(key="surv.year", mod="+", value=function() 1) %>%
          seize(resource="TN", amount=1) %>% 
          timeout(task = t_img) %>% #wachttijd imaging?
          release(resource="TN", amount=1) %>%
-         rollback(target=6, times=Inf),
+         rollback(target=9, times=Inf),
        
        #Event 2: True Positive / False Positive
-       #Event 3: False Negative? 
-       #Event 4: Additional imaging?
+       trajectory() %>%
+         branch(option=function() get_attribute(sim, "biopt.DF"), continue=c(T,T), #make it a function??
+                #negative biopsy - rollback to FUP.event 
+                trajectory() %>%
+                  set_attribute(key="surv.year", mod="+", value=function() 1) %>%
+                  seize(resource="FP", amount=1) %>% 
+                  timeout(task = t_img) %>% 
+                  timeout(task  = t_biopsy) %>%
+                  release(resource="FP", amount=1) %>%
+                  rollback(target=11, times=Inf),
+                
+                #positive biopsy 
+                trajectory() %>%  
+                  set_attribute(key="surv.year", mod="+", value=function() 1) %>%
+                  seize(resource="TP", amount=1) %>% 
+                  timeout(task = t_img) %>% 
+                  timeout(task = t_biopsy) %>% 
+                  release(resource="TP", amount=1) %>%
+                  set_attribute(key="state_DU", values=1)
+         ),
        
+       #Event 3: Additional imaging?
+       trajectory() %>%
+         timeout(task = t_img) %>%
+         rollback(target=6, times=Inf),
        
-       ) %>% 
+       #Event 4: False Negative? 
+       trajectory() %>%
+         seize(resource="FP", amount=1) %>% 
+         timeout(task = t_img) %>%
+         release(resource="FP", amount=3) %>%
+         set_attribute(key="state_UU", values=1, mod = "+") %>%
+         #set_attribute(key="surv.year", mod="+", value=function() 1) %>%
+         
+         branch(option = function() fn_img_event_UU(get_attribute(sim, "mod")), continue = c(T,T), #different fn_img_event function?? Denk het wel om van UU naar US te gaan // needs to be function that keeps track of time to move to state US
+                #0 to skip branch to undetected symptomatic
+                #undetected unsymptomatic UU
+                trajectory() %>%
+                  #set_attribute(key="state_UU", mod="+", values = 1) %>%
+                  timeout(task = t_img) %>%
+                  set_attribute(key="surv.year", mod="+", values = 1) %>%
+                  rollback(target=4),
+                
+                #undetected symptomatic
+                trajectory() %>%
+                  timeout(task = t_img) %>%
+                  set_attribute(key="state_US", values=1, mod = "+")
+                ) %>%
+         
+         #detected (un)symptomatic
+         set_attribute(key="state_DS", values=1, mod = "+") %>% 
+         branch(option=function() get_attribute(sim, "biopt.US"), continue=c(T,T), #different biopsy value?? #make it a function based on probability?
+                #negative biopsy - should this even be possible?
+                trajectory() %>%
+                  #set_attribute(key="state_US", mod="+", values = 1) %>%
+                  timeout(task  = t_biopsy) %>%
+                  rollback(target=5),
+                
+                #positive biopsy = 
+                trajectory() %>%  
+                  set_attribute(key="state_DS", values = 1, mod = "+") %>%
+                  timeout(task = t_biopsy)
+         ) %>%
+         timeout(task = t_fn)
+  ) %>%
+  seize(resource="Treatment", amount=1)
 
-fup.model <- trajectory() %>%
-
-  
-
-
-
+ 
 # Visualize 
-plot(bsc.model)
+plot(fup.trj)
+
+model %>%
+  export_svg() %>%
+  charToRaw %>%
+  rsvg_pdf("graph.pdf")
+
