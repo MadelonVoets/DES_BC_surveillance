0612f1012b6caa6b1d648237458ea9f0688f71b7
m7668417 <m.m.voets@utwente.nl>
2024-03-12 13:54:52 +0100

    added master

---
+++ 0_master.R
@@ -0,0 +1,19 @@
+##-----------------------------------------------------------------------------------------##
+##                                                                                         ##
+##            Developing a simulation model to support implementation of                   ##
+##             artificial intelligence in breast cancer surveillance                       ##
+##                                                                                         ##
+##                                                                                         ##
+##                                                                                         ##
+##-----------------------------------------------------------------------------------------##
+#
+#
+# # This script contains the following sections:
+# 1. Init             defines the environment for the analysis
+# 2. Parameters       defines the parameters used to define the trajectory
+# 3. Functions        defines functions that are used to implement and run the simulation
+# 4. Trajectory       defines the model structure (trajectory)
+# 5. Simulation       defines and runs the simulations, including analysis of the outcomes
+#
+# This script was written using R Studio version XX and R version . The versions
+# of the packages used are specified in the 1_init section.
