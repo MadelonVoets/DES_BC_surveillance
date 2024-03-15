40c73e13fa55a09eed74317884e761711d8f8ade
m7668417 <m.m.voets@utwente.nl>
2024-03-15 14:47:37 +0100

    cbs data added

--- 1_init.R
+++ 1_init.R
@@ -8,6 +8,8 @@ library(simmer);                 #Version 4.4.6.3
 library(simmer.plot);            #Version 0.1.18
 library(dplyr);                  #Version 1.1.4
 library(rsvg);                   #Version 2.6.0
+library(readxl)
+library(readr)
 #library(DiagrammeRsvg)
 
 sessionInfo()
@@ -16,5 +18,10 @@ sessionInfo()
 source("getSingleAttribute.R", echo=TRUE);
 source("getMultipleAttributes.R", echo=TRUE);
 
-# Load the dataset
-load("")
+#rel_path <= "~/AMICUS/Stukken/4_Simulation model/model/"
+
+# Load the INFLUENCE risk matrix
+inf_matrix <- read_excel("INFLUENCE_matrix.xlsx")
+mortality_data <- read_delim("cbs_sterftekans18-100.csv", 
+                             + delim = ";", escape_double = FALSE, trim_ws = TRUE)
+mortality_data <- as.data.frame(mortality_data)
