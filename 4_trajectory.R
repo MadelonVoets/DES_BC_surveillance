c89fcccf0c9e27a099ec28c1c55be98bb46967b9
m7668417 <m.m.voets@utwente.nl>
2024-04-18 12:18:50 +0200

    patient characteristics

--- 4_trajectory.R
+++ 4_trajectory.R
@@ -28,23 +28,68 @@ cs.trj <- trajectory() %>%
   timeout(task = t_wbimaging) %>%
   release(resource = "WB.Imaging")
 
+# MAIN TRAJECTORY 
 fup.trj <- trajectory() %>% 
+  
+  #patient characteristics
+  set_attribute(key="Sex", value=function() fn_sex()) %>%
+  set_attribute(key=c("Age","A.grp"), value=function() fn_age()) %>%
+  set_attribute(key="Grade", value=function() fn_grade()) %>%
+  set_attribute(key="Stage", value=function() fn_stage()) %>%
+  set_attribute(key="Nstatus", value=function() fn_nstatus()) %>%
+  set_attribute(key="Multif", value=function() fn_multif()) %>%
+  set_attribute(key="Sur", value=function() fn_sur()) %>%
+  set_attribute(key="Chemo", value=function() fn_chemo()) %>%
+  set_attribute(key="Radio", value=function() fn_radio()) %>%
+  set_attribute(key="Horm", value=function() fn_horm()) %>%
+  set_attribute(key="Antiher2", value=function() fn_antiher2()) %>%
+  
+  set_attribute(key="t_LRR", value=function() fn_t_to_tumour(fn_risk(vector = c(get_attribute(sim, "A.grp"),
+                                                                get_attribute(sim, "Grade"),
+                                                                get_attribute(sim, "Stage"),
+                                                                get_attribute(sim, "Nstatus"),
+                                                                get_attribute(sim, "Multif"),
+                                                                get_attribute(sim, "Sur"),
+                                                                get_attribute(sim, "Chemo"),
+                                                                get_attribute(sim, "Radio"),
+                                                                get_attribute(sim, "Horm"),
+                                                                get_attribute(sim, "Antiher2")), rec=1))) %>%
+  set_attribute(key="t_DM", value=function() fn_t_to_tumour(fn_risk(vector = c(get_attribute(sim, "A.grp"),
+                                                                               get_attribute(sim, "Grade"),
+                                                                               get_attribute(sim, "Stage"),
+                                                                               get_attribute(sim, "Nstatus"),
+                                                                               get_attribute(sim, "Multif"),
+                                                                               get_attribute(sim, "Sur"),
+                                                                               get_attribute(sim, "Chemo"),
+                                                                               get_attribute(sim, "Radio"),
+                                                                               get_attribute(sim, "Horm"),
+                                                                               get_attribute(sim, "Antiher2")), rec=2))) %>%
+  
+  
+  #set_attribute(key="Age", value=function() setAge(get_attribute(bsc.sim, "Male"))) %>%
+  #set_attribute(key="Tx1.Response", value=function() setTx1(get_attribute(bsc.sim, "Poor"))) %>%
+  #set_attribute(key="Tx2.Response", value=function() setTx2(get_attribute(bsc.sim, "Tx1.Response"))) %>%
+  #set_attribute(key="Tx1.Cycles", value=0) %>%
+  #set_attribute(key="Tx2.Cycles", value=0) %>%
+  #set_attribute(key="C", value=0) %>%
+  #set_attribute(key="E", value=0) %>%
+  
   set_attribute(keys = "start_time", values = function() now(.env = sim)) %>% 
-  renege_in(t = function() now(.env = sim) + fn_days_death_oc, out = out.trj) %>% 
+  renege_in(t = function() now(.env = sim) + fn_days_death_oc(age = get_attribute(sim, "Age"), sex = get_attribute(sim, "Sex"), mortality_data), out = out.trj) %>% 
 
   #set_attribute(key="state", "DF") %>%
   set_attribute(key="state_DF", mod="+", value=function() 1) %>%
   
    # Time to first imaging event
-  #timeout(task = t_first_fup) %>% #distribution to follow-up event
+  timeout(task = function() rnorm(1, mean = 0, sd = 15.5)) %>% #distribution to follow-up event
   
   # Follow-up imaging event
   seize(resource = "Imaging") %>%
-  set_attribute(
-    keys = c("mod", "cost"),
-    values = function() fn_img_mod(at = now(.env = sim)),
-    mod = '+'
-  ) %>%
+  #set_attribute(
+  #  keys = c("mod", "cost"),
+  #  values = function() fn_img_mod(at = now(.env = sim)),
+  #  mod = '+'
+  #) %>%
   #timeout(task = t_fup) %>%
   release(resource = "Imaging") %>%
   
