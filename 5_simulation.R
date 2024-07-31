93d00663ef7aee99a92677414c8d8bb069e01ce6
m7668417 <m.m.voets@utwente.nl>
2024-07-31 16:59:42 +0200

    added all resources

--- 5_simulation.R
+++ 5_simulation.R
@@ -6,15 +6,25 @@ mon.patients <- 2;    # level of monitoring (see add_generator)
 
 # Run simulation for the best standard care (bsc)
 sim <- simmer() %>%
+  #main
   add_resource(name="Imaging", capacity=Inf, mon=F) %>%
-  #add_resource(name="FU1", capacity=Inf, mon=F) %>%
-  #add_resource(name="Tx2", capacity=Inf, mon=F) %>%
-  #add_resource(name="FU2", capacity=Inf, mon=F) %>%
+  add_resource(name="TN", capacity=Inf, mon=F) %>%
+  add_resource(name="A.Imaging", capacity=Inf, mon=F) %>%
+  add_resource(name="FP", capacity=Inf, mon=F) %>%
+  add_resource(name="TP", capacity=Inf, mon=F) %>%
+  add_resource(name="FN", capacity=Inf, mon=F) %>%
+  add_resource(name="UU", capacity=Inf, mon=F) %>%
+  add_resource(name="US", capacity=Inf, mon=F) %>%
+  add_resource(name="DS", capacity=Inf, mon=F) %>%
+  #treat.trj
+  add_resource(name="LRR.treatment", capacity=Inf, mon=F) %>%
+  #DM
+  add_resource(name="WB.Imaging", capacity=Inf, mon=F) %>%
+  add_resource(name="NC.treatment", capacity=Inf, mon=F) %>%
   add_generator(name_prefix="Patient", trajectory=fup.trj, distribution=at(rep(x=0, times=n.patients)), mon=mon.patients);
 
 sim %>% run();
 
 sim.out <- get_mon_attributes(sim);       # retrieve the monitor object
 
-
-#View(getMultipleAttributes(c("Tx1.Event","Tx1.Cycles","Tx2.Event","Tx2.Cycles","C","E"), bsc.out)); 
+View(getMultipleAttributes(c("Age","Grade","Stage","Nstatus","Multif","Chemo", "Sur", "Radio", "Horm", "Antiher2", "surv.year", "start.imaging"), sim.out))
