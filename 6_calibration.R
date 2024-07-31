5b73d65e4885334502988c75702212d4ff0b8b64
m7668417 <m.m.voets@utwente.nl>
2024-07-31 17:00:08 +0200

    ispor abstract

--- 6_calibration.R
+++ 6_calibration.R
@@ -3,7 +3,9 @@ library(ggstatsplot)
 library(tidyr)
 
 #Test population
-test <- fn_gen_pt(5000)
+test <- fn_gen_pt(100000)
+#save(test, file= "test.Rda")
+#load("test.Rda")
 sub.test <- subset(test, select = -c(ID, age, age.grp, t.DM, t.LRR, vdt_lrr, vdt_dm)) 
 test.list <- as.list(sub.test)
 
@@ -62,12 +64,12 @@ check_calibration <- function(test_data_list, calibration_probs_list) {
     
     # Check if the proportions are within 10% of the calibration parameters
     check_results <- mapply(function(test_prop, cal_prob) {
-      abs(test_prop - cal_prob) <= 0.05 #* cal_prob
+      abs(test_prop - cal_prob) <= 0.02 #* cal_prob
     }, test_proportions, calibration_probs)
     
     # Print the appropriate message based on the checks
     if (all(check_results)) {
-      print(paste("Parameter", i, ": Distributions within 5%"))
+      print(paste("Parameter", i, ": Distributions within 2%"))
     } else {
       for (j in seq_along(check_results)) {
         if (!check_results[j]) {
@@ -89,8 +91,9 @@ risk.list <- apply(test, 1, function(row) {
   
 })
 
+
 risks <- as.data.frame(do.call(rbind,risk.list))
-risk.index <- rownames(risks)
+#risk.index <- rownames(risks)
 risks$con_1 <- risks$c_lrr1
 risks$con_2 <- risks$c_lrr2 - risks$c_lrr1
 risks$con_3 <- risks$c_lrr3 - risks$c_lrr2
@@ -117,7 +120,7 @@ plt <- plt +
   #add labels and titles
   labs(
     x = "Year",
-    y = "Cumulative Risk Calibration(%)"
+    y = "Conditional Annual Risk Calibration(%)"
   ) + 
   #customizations
   theme( 
@@ -166,9 +169,9 @@ inf_ref <- data.frame(
 
 inf_pred <- data.frame(
   Year = factor(1:5),
-  Mean = c(0.445, 0.983, 0.839, 0.787, 0.496),        # Replace these with your actual mean values
-  LowerIQR = c(0.154, 0.423, 0.594, 0.450, 0.336),      # Replace these with your actual lower IQR values
-  UpperIQR = c(0.453, 1.19, 1.018, 0.958, 0.610)     # Replace these with your actual upper IQR values
+  Mean = c(0.446, 0.982, 0.834, 0.794, 0.492),        # Replace these with your actual mean values
+  LowerIQR = c(0.151, 0.414, 0.578, 0.451, 0.330),      # Replace these with your actual lower IQR values
+  UpperIQR = c(0.456, 1.193, 1.019, 0.957, 0.602)     # Replace these with your actual upper IQR values
 )
 
 inf_ref$Group <- 'Observed'
@@ -182,7 +185,7 @@ inf_comb <- rbind(inf_ref, inf_pred)
 ggplot(inf_comb, aes(x = Year, y = Mean, color = Group)) +
   geom_errorbar(aes(ymin = LowerIQR, ymax = UpperIQR), width = 0.2, position = position_dodge(0.3)) +
   geom_point(position = position_dodge(0.3), size = 3) +
-  labs(title = "Combined Plot of Two Datasets", x = "Year", y = "Value") +
+  labs(title = "Combined Plot of Two Datasets for LRR", x = "Year", y = "Value") +
   theme_minimal() +
   scale_color_manual(values = c("Observed" = "blue", "Predicted" = "red"))
 # YEAR 4 LIJKT WAT HOOG
@@ -191,9 +194,6 @@ ggplot(inf_comb, aes(x = Year, y = Mean, color = Group)) +
 ##
 prop.table(table(test$t.DM))
 prop.table(table(test$t.LRR))
-
-## BC DEATH 
-#check "oligo.R"
 
 ## VDT // SYMPTOMATIC CALIBRATION 
 #test$t_det <- ifelse(test$vdt_lrr == 0, 0, fn_days_symptomatic(vdt = test$vdt_lrr, data = df_patient, model = symp_cox_model))
@@ -228,26 +228,126 @@ routine_visit_times <- c(0, 365, 730, 1095, 1460, 1825)
 dynamic_visit_times <- fn_dynamic_visits(routine_visit_times)
 
 # Apply the function to each row in the dataframe
-test$detection_type <- mapply(fn_detection_type, test$dfi, test$t_symp) #, routine_visit_times)
+test$detection_stat <- mapply(fn_detection_type_static, test$dfi, test$t_symp) #, routine_visit_times)
+
+## VOLUME DOUBLING TIME
+ggplot(CDF_values, aes(x=VDT, y=CP)) + geom_point() + xlab("Volume Doubling Time (days)") + ylab("Cumulative Probability") + geom_errorbar(aes(xmin=min, xmax=max), width=0.01) +xlim(0,800)+
+  stat_function(
+    fun = function(x) pnorm(x, mean = mean.norm.vdt, sd = sd.norm.vdt),
+    aes(color = "Fitted"),
+    linetype = "solid") +
+  scale_color_manual(values = c("red", "blue", "green", "orange", "pink"))
+
+#subset for vdt > 0, sorted ascending with added cumulative probability
+sub.vdt <- subset(test, t.LRR > 0) 
+sub.vdt$n <- 1
+sub.vdt <- sub.vdt[order(sub.vdt$vdt_lrr),]
+sub.vdt <- sub.vdt %>%
+  mutate(
+    #'Count CDF' = cumsum(Counts) / sum(Counts),
+    'CP' = cumsum(n) / sum(n)
+  )
+
+#plot pred vdt (max =107) against fitted vdt
+ggplot(data.frame(x = c(0, 120)), aes(x = x)) + xlim(c(0, 120)) + ylim(0, 1) +
+  stat_function(
+    fun = function(x) pnorm(x, mean = mean.norm.vdt, sd = sd.norm.vdt),
+    aes(color = "Fitted"),
+    linetype = "solid") +
+  geom_point(data = sub.vdt, 
+           mapping = aes(x = vdt_lrr, y = CP))
 
 
+## RISK OF DISTANT METASTASIS
+risk.list.dm <- apply(test, 1, function(row) {
+  vector <- c(row["age.grp"], row["grade"], row["stage"], row["nstatus"], row["multif"], row["sur"], row["chemo"], row["radio"], row["horm"], row["antiher2"])
+  risk <- fn_risk(vector, inf_matrix, rec=2)
+  return(risk)
+  
+})
+
+#conditional risk
+risks.dm <- as.data.frame(do.call(rbind,risk.list.dm))
+#risk.index <- rownames(risks)
+risks.dm$con_1 <- risks.dm$c_DM1
+risks.dm$con_2 <- risks.dm$c_DM2 - risks.dm$c_DM1
+risks.dm$con_3 <- risks.dm$c_DM3 - risks.dm$c_DM2
+risks.dm$con_4 <- risks.dm$c_DM4 - risks.dm$c_DM3
+risks.dm$con_5 <- risks.dm$c_DM5 - risks.dm$c_DM4
+
+inf_pred_dm <- data.frame(
+  Year = factor(1:5),
+  Mean = c(0.6858, 1.3696, 1.4887, 1.2726, 0.9723),        # Replace these with your actual mean values
+  LowerIQR = c(0.2500, 0.5101, 0.5691, 0.5065, 0.906),      # Replace these with your actual lower IQR values
+  UpperIQR = c(0.8389, 1.7098, 1.8924, 1.6421, 1.2742)     # Replace these with your actual upper IQR values
+)
+
+inf_pred_dm$Group <- 'Predicted'
+
+ggplot(inf_pred_dm, aes(x = Year, y = Mean, color = Group)) +
+  geom_errorbar(aes(ymin = LowerIQR, ymax = UpperIQR), width = 0.2, position = position_dodge(0.3)) +
+  geom_point(position = position_dodge(0.3), size = 3) +
+  labs(title = "Risks of DM for 100.000 simulated patients per year of follow-up", x = "Year", y = "Conditional Annual Risk of DM") +
+  theme_minimal() #+
+  #scale_color_manual(values = c("Observed" = "blue", "Predicted" = "red"))
+
+## BC DEATH 
+#check "oligo.R"
+sub.dm <- subset(test, t.DM > 0)
+sub.dm$type <- ifelse(runif(nrow(sub.dm)) < p.oligo, 1, 2) #1 oligo , 2 dm
+
+sub.dm$ttod <- apply(sub.dm,1, function(row) fn_day_death_bc(row["type"]))
+
+surv.pred.o <- survfit(Surv(ttod) ~ 1, data = sub.dm[sub.dm$type == 1,])
+surv.pred.dm <- survfit(Surv(ttod) ~ 1, data = sub.dm[sub.dm$type == 2,])
 
 
+# Plot together
+plot(surv.observed.w, col = "blue", main = "Fitted parametric survival curves", xlab = "Time since diagnosis (days)", ylab = "Survival probability", conf.int = F)
+lines(surv.observed.w, lwd=2, col = "blue", conf.int = F)
+lines(surv.observed.o, lwd=1, col='black', conf.int = F, linetype="dashed")
+lines(surv.observed.w.dm, lwd=2, col='red', conf.int = F)
+lines(surv.observed.dm, lwd=1, col='black', conf.int = F)
+
+lines(surv.pred.dm, lwd=1, col='green', conf.int = F)
+lines(surv.pred.o, lwd=1, col='yellow', conf.int = F)
+
+legend("topright", legend = c("Fit Oligo", "Observed", "Fit >3 DM", "Sim >3 DM", "Sim Oligo"), col = c("blue", "Black", "red", "green", "yellow"), lty = 1:2, bty = "n")
+
+#shape = fit.wei$scale, scale = exp(fit.wei$coefficients))
+sub.dm$event <- 1
+fit.o <- survreg(Surv(ttod, event) ~ 1,data = sub.dm[sub.dm$type == 1,], dist = "weibull")
+fit.dm <- survreg(Surv(ttod, event) ~ 1,data = sub.dm[sub.dm$type == 2,], dist = "weibull")
 
 
+plot(surv.observed.o, col = "black", main = "Fitted parametric survival curves", xlab = "Time since diagnosis (days)", ylab = "Survival probability")
+lines(surv.observed.o[1], lwd=1)
+lines(surv.observed.dm, lwd=1, col='black', conf.int = T)
+pct <- seq(.01,.99,by=.01)
+lines(predict(fit.o,type="quantile",p=pct)[1,],1-pct,col="red",lwd=1)
+lines(predict(fit.dm,type="quantile",p=pct)[1,],1-pct,col="blue",lwd=1)
+
+#Komogorov-Smirnov tests
+ks_test1 <- ks.test(oligo$time_to_death, "pweibull", shape = fit.o$scale, scale = exp(fit.o$coefficients))
+ks_test2 <- ks.test(dm$time_to_death, "pweibull", shape = fit.dm$scale, scale = exp(fit.dm$coefficients))
+
+## SYMPTOMATIC
+sub.vdt$vdt_lrr <- ifelse(sub.vdt$vdt_lrr == 0, fn_trnorm(1, mean.norm.vdt, sd.norm.vdt,1,21.55), sub.vdt$vdt_lrr)
+# Apply the function to each row in the dataframe
+sub.vdt$detection_stat <- mapply(fn_detection_type_static, sub.vdt$dfi, sub.vdt$t_symp) #, routine_visit_times)
+
+## LRR SUBSET
+test.lrr <- subset(test, t.LRR > 0)
+test.lrr <- test.lrr %>%
+  mutate(jr = t.LRR / 365)
+
+ggplot(test.lrr) + aes(x = jr) + geom_bar()
+#combined plot risk and incidence??
 
 
-
-
-
-
-
-
-
-
-
-
-
+## DM SUBSET
+test.dm <- subset(test, t.DM > 0)
+ggplot(test.dm) + aes(x = t.DM) + geom_bar()
 
 
 
